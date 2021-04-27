643c930f8c8bc22c4a5fc73c38db9117c8488f4c
xiaoao-xyz <ao.xiao@xyzrobotics.ai>
2021-04-28 07:30:13 +0800

    add pytest

---
+++ learn_note/framewoek/pytest/1_test_demo.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    return x+y
+
+def test_add():
+    assert add(1, 10) == 11
+    assert add(1, 1) == 2
+    assert add(1, 99) == 100
+
+class TestDemo():
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+    
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
+
+    
---
+++ learn_note/framewoek/pytest/4_test_fixture.py
@@ -0,0 +1,33 @@
+import pytest
+
+@pytest.fixture()
+def login():
+    print("\n执行登录")
+    return ('tom', '123')
+
+@pytest.fixture()
+def operate():
+    print("\n登陆后的操作")
+
+@pytest.fixture(scope="session") # session > module > class > function
+def boring_func():
+    print("\nThis is boring func")
+
+def test_case1(login, operate):
+    print(login)
+    print("\ntest_case1，需要登陆")
+
+def test_case2(boring_func):
+    print("\ntest_case2, 不需要登陆")
+
+def test_case3(login):
+    print('\n'+str(login))
+    print("\ntest_case3, 需要登陆")
+
+# TODO 对于希望全局使用的函数，可以放在conftest.py里
+"""
+注意事项：
+    1. conftest.py具有唯一性
+    2. 在根目录下或是某一个package下面运行，作用范围也因此不同
+        
+"""
