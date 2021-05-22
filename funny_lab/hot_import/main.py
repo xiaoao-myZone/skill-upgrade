6b73a1a6b25867e8ed32b4735be53b22d14d4fd1
xiaoao-xyz <ao.xiao@xyzrobotics.ai>
2021-05-22 13:57:35 +0800

    update

--- funny_lab/hot_import/main.py
+++ funny_lab/hot_import/main.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from imp import reload
 import hot_import
+
+def print_data():
+    print(data)
+
+
 while True:
     ans = input("enter to continue")
     if not ans:
         reload(hot_import)
         from hot_import import data
-        print(data)
+        global data
+        # print(data)
+        print_data()
     else:
         break
 
@@ -17,5 +24,6 @@ Conclusion:
     1. 当hot_import中所读的json文件被修改时， 在不重启进程的情况下， import是不会再执行hot_import.py中的内容的
     2. 需要调用reload， 但是入reload好像只有python2有
     3. python3中reload在imp这个lib里
+    4. reload后需要重新import 
 
 """
