24bbc11b915748dc6dded6f27c718b38a05b77fe
xiaoao-xyz <ao.xiao@xyzrobotics.ai>
2021-08-30 14:24:01 +0800

    update decorator and epoll

--- python_base/decorator_note/decorator_for_method.py
+++ python_base/decorator_note/decorator_for_method.py
@@ -1,5 +1,6 @@
 def log(func):
     def _wrapper(self, *args, **kwargs):
+        print("log")
         print("Class: %s" % self.__class__)
         print("function: %s" % func.__name__)
         print(' '.join(map(str,args)))
@@ -10,13 +11,25 @@ def log(func):
 
     return _wrapper
 
+def anther_log(func):
+    def _wrapper(self, *args, **kwargs):
+        print("anther_log")
+        ret=func(self, *args, **kwargs)
+        return ret
+    return _wrapper
+
 class Test(object):
     def __init__(self):
         pass
     
     @log
+    @anther_log
     def add(self, a, b):
         return a+b
 
 t = Test()
 t.add(1, 5)
+
+"""
+1. 装饰器是从上至下运行的，上面的装饰器先运行
+"""
